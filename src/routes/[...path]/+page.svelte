b75a1bf0ebc8f98ad5a73c5e9ba569bedb2100db
Walter Leeper <wleeper50@hotmail.com>
2023-02-10 01:12:41 -0600

    404 handling

---
+++ src/routes/[...path]/+page.svelte
@@ -0,0 +1,19 @@
+<body>
+  <h1 class="blue">404</h1>
+  <p class="cyan">Resource not found</p>
+</body>
+
+<style>
+  body {
+    padding-top: 2em;
+    background-color: #222;
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  h1 {
+    font-size: 4em;
+  }
+  p {
+    font-size: 3em;
+  }
+</style>
