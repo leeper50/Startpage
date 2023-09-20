5b1eb77c9c720c030e2a5cb5cb55f982e12ba869
Walter Leeper <wleeper13@outlook.com>
2023-09-20 16:17:25 -0500

    improve tests

--- src/tests/search.test.ts
+++ src/tests/search.test.ts
@@ -1,9 +1,17 @@
-import { _API_KEY, DELETE, POST, PUT } from "../routes/api/search/+server";
+import * as Server from "../routes/api/search/+server";
+import * as SlugGet from "../routes/api/search/[q]/+server";
 import { expect, test } from "vitest";
 
-test("Post - (no engine)", async () => {
+test("Get with empty string", async () => {
+  const res = await Server.GET();
+  const url = await res.text();
+  expect(url).toBe("https://duckduckgo.com");
+});
+
+test("Get", async () => {
   const queries = new Map<string, string>([
     ["test", "https://duckduckgo.com/?t=ffab&q=test"],
+    ["c#", "https://duckduckgo.com/?t=ffab&q=c%23"],
     ["-g", "https://www.google.com"],
     ["-g e", "https://www.google.com/search?q=e"],
     ["-pcp", "https://pcpartpicker.com"],
@@ -16,71 +24,33 @@ test("Post - (no engine)", async () => {
     ["-NOTREALCOMMAND", ""],
   ]);
   queries.forEach(async (result, search) => {
-    const res = await POST({
-      request: {
-        json: () => {
-          return {
-            text: search,
-          };
-        },
-      },
-    });
+    const res = await SlugGet.GET({ params: { q: search } });
     const url = await res.text();
     expect(url).toBe(result);
   });
 });
 
-test("Post - (engine provided)", async () => {
-  const queries = new Map<string, string>([
-    ["test", "https://www.google.com/search?q=test"],
-    ["-g", "https://www.google.com"],
-    ["-g e", "https://www.google.com/search?q=e"],
-    ["-pcp", "https://pcpartpicker.com"],
-    ["-pcp e", "https://pcpartpicker.com/search/?q=e"],
-    ["-y", "https://www.youtube.com"],
-    ["-y test", "https://www.youtube.com/results?search_query=test"],
-    ["", "https://www.google.com/search?q="],
-    ["-", ""],
-    ["--", ""],
-    ["-NOTREALCOMMAND", ""],
-  ]);
-  queries.forEach(async (result, search) => {
-    const res = await POST({
-      request: {
-        json: () => {
-          return {
-            text: search,
-            engine: "Google",
-          };
-        },
-      },
-    });
-    const url = await res.text();
-    expect(url).toBe(result);
-  });
-});
-
-test("Put", async () => {
+test("Post", async () => {
   const queries = new Map<string, { url?: any; searchable?: any }>([
     [
       "-test1",
       {
         url: "https://test.com",
-        searchable: false,
+        searchable: "false",
       },
     ],
     [
       "test2",
       {
         url: "https://test.com",
-        searchable: false,
+        searchable: "false",
       },
     ],
     [
       "-test3",
       {
         url: 5,
-        searchable: false,
+        searchable: "false",
       },
     ],
     [
@@ -100,25 +70,75 @@ test("Put", async () => {
     [
       "-test7",
       {
-        searchable: true,
+        searchable: "true",
       },
     ],
   ]);
   const results = new Map<string, string>([
-    ["-test1", `PUT - Added: -test1: ${JSON.stringify(queries.get("-test1"))}`],
-    ["test2", `PUT - test2 was not added`],
-    ["-test3", `PUT - -test3 was not added`],
-    ["-test4", `PUT - -test4 was not added`],
-    ["-test5", `PUT - -test5 was not added`],
-    ["-test6", `PUT - -test6 was not added`],
-    ["-test7", `PUT - -test7 was not added`],
+    [
+      "-test1",
+      `POST - Added: -test1: ${JSON.stringify(queries.get("-test1"))}`,
+    ],
+    ["test2", `POST - test2 was not added`],
+    ["-test3", `POST - -test3 was not added`],
+    ["-test4", `POST - -test4 was not added`],
+    ["-test5", `POST - -test5 was not added`],
+    ["-test6", `POST - -test6 was not added`],
+    ["-test7", `POST - -test7 was not added`],
   ]);
   queries.forEach(async (value, key) => {
-    const res = await PUT({
+    const res = await Server.POST({
       request: {
         headers: {
           get: () => {
-            return _API_KEY;
+            return Server._API_KEY;
+          },
+        },
+        json: () => {
+          return { [key]: value };
+        },
+      },
+    });
+    const url = await res.text();
+    expect(url).toBe(results.get(key));
+  });
+});
+
+test("Put", async () => {
+  const queries = new Map<string, { url?: any; searchable?: any }>([
+    [
+      "-g",
+      {
+        url: "https://duckduckgo.com",
+        searchable: "true",
+      },
+    ],
+    [
+      "-g",
+      {
+        url: "https://google.com",
+        searchable: "true",
+      },
+    ],
+    [
+      "-notacommand",
+      {
+        url: "https://test.com",
+        searchable: "false",
+      },
+    ],
+  ]);
+  const results = new Map<string, string>([
+    ["-g", `PUT - Changed: -g: ${JSON.stringify(queries.get("-g"))}`],
+    ["-d", `PUT - Changed: -d: ${JSON.stringify(queries.get("-d"))}`],
+    ["-notacommand", `PUT - Not Present: -notacommand`],
+  ]);
+  queries.forEach(async (value, key) => {
+    const res = await Server.PUT({
+      request: {
+        headers: {
+          get: () => {
+            return Server._API_KEY;
           },
         },
         json: () => {
@@ -135,13 +155,14 @@ test("Delete", async () => {
   const queries = new Map<string, string>([
     ["-pcp", "DELETE - -pcp was removed"],
     ["-NOTAREALCOMMAND", "DELETE - -NOTAREALCOMMAND was not present"],
+    ["-", "DELETE - - was not present"],
   ]);
   queries.forEach(async (result, search) => {
-    const res = await DELETE({
+    const res = await Server.DELETE({
       request: {
         headers: {
           get: () => {
-            return _API_KEY;
+            return Server._API_KEY;
           },
         },
         json: () => {
