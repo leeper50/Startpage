267c266680ebd5373f86ff794b1450d1cf76eae7
Walter Leeper <wleeper13@outlook.com>
2024-01-30 16:17:01 -0600

    added skeleton components lib

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-node";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+  preprocess: [preprocess(), vitePreprocess({})],
 
   kit: {
     adapter: adapter(),
