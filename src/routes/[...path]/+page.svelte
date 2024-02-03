77024283bdae4267a8332a10d269f9260be17dea
Walter Leeper <wleeper13@outlook.com>
2024-02-03 17:31:01 -0600

    error page updated

---
+++ src/routes/[...path]/+page.svelte
@@ -0,0 +1,5 @@
+<div class="h-full pt-2 bg-surface-500 text-center text-primary-500 text-6xl">
+  <h1 class="text-8xl pb-4">404</h1>
+  <p class="text-secondary-500 pb-4">Resource not found</p>
+  <a href="/" class="hover:text-error-500 underline">How bout you go on home</a>
+</div>
