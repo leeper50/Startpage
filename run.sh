eb3de7cf422e010a29a2ddef6b3b3528292719b4
Walter Leeper <wleeper13@outlook.com>
2024-02-03 12:09:25 -0600

    fix restart problem with docker

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 # Build out prisma db
-mkdir -p prisma &&\
-mv schema.prisma prisma &&\
-cd prisma &&\
+set -e
+mkdir -p prisma
+cp schema.prisma prisma
+cd prisma
 npm run prisma
-rm schema.prisma &&\
-cd /app &&\
+rm schema.prisma
+cd /app
 # Start server
 node index.js
