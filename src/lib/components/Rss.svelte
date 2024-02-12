d5e80f1d029741047cc5902466718b7e9bdd1a49
Walter Leeper <wleeper13@outlook.com>
2024-02-12 16:38:59 -0600

    minimal format has rss support

--- src/lib/components/Rss.svelte
+++ src/lib/components/Rss.svelte
@@ -1,15 +1,40 @@
 <script lang="ts">
+  export let minimal = true;
   export let data = [{ title: "", url: "", site: "" }];
 </script>
 
-<p>NEWS</p>
-{#each data as { title, url, site }}
-  <a href={url} class="h-8 inline-block overflow-hidden whitespace-nowrap text-ellipsis hover:text-gray-400" {title}>
-    <img
-      alt=""
-      class="w-8 h-8 inline-block align-top grayscale-[60%] rounded"
-      src={`/images/${site.slice(8, site.indexOf("/", 9))}`}
-    />
-    {title}
-  </a>
-{/each}
+{#if !minimal}
+  <p>NEWS</p>
+  {#each data as { title, url, site }}
+    <a
+      href={url}
+      class="h-8 inline-block overflow-hidden whitespace-nowrap text-ellipsis hover:text-gray-400"
+      {title}
+    >
+      <img
+        alt=""
+        class="w-8 h-8 inline-block align-top grayscale-[60%] rounded"
+        src={`/images/${site.slice(8, site.indexOf("/", 9))}`}
+      />
+      {title}
+    </a>
+  {/each}
+{:else}
+<div class="flex flex-col border-4 border-surface-400 p-2 grow">
+  <div class="flex">
+    <span class="text-error-500 pr-2">~</span>
+    <span class="text-primary-500 pr-2">$</span>
+    <span class="text-surface-300 p-0">~/</span>
+    <span class="text-success-500 p-0">News</span>
+  </div>
+  {#each data as { title, url }}
+    <a
+      href={url}
+      class="h-8 overflow-hidden whitespace-nowrap text-ellipsis text-primary-400 hover:text-error-500"
+      {title}
+    >
+      {title}
+    </a>
+  {/each}
+</div>
+{/if}
--- src/routes/(root)/+page.svelte
+++ src/routes/(root)/+page.svelte
@@ -10,10 +10,12 @@
 
 {#if !user || user.fancy === false}
   <div
-    class="flex bg-surface-500 h-full overflow-auto justify-center lg:items-center font-bold"
+    class="flex bg-surface-500 h-full overflow-auto justify-center md:items-center font-bold"
   >
     <div class="flex flex-col container text-2xl gap-4">
-      <div class="flex gap-4 border-4 border-surface-400 items-center p-2">
+      <div
+        class="flex flex-wrap gap-4 border-4 border-surface-400 items-center p-2"
+      >
         <div class="flex h-min">
           <span class="text-primary-500">user</span>
           <span class="text-surface-300">@</span>
@@ -26,19 +28,33 @@
           <Searchbar />
         </div>
       </div>
-      <div class="flex flex-wrap gap-4">
-        <Linkbox linkList={pageData} />
+      <div class="flex flex-wrap w-full text-2xl text-gray-300 gap-2">
+        {#if user.rssVisibility}
+          <div class="flex flex-col max-w-3xl grow gap-4 overflow-hidden">
+            {#if data.valid}
+              <Rss minimal={true} data={data.items} />
+            {:else}
+              <p>
+                Problem with rss config!
+                <br />Check your url and key.
+              </p>
+            {/if}
+          </div>
+        {/if}
+        <Linkbox minimal={true} linkList={pageData} />
       </div>
+      <!-- <div class="flex flex-wrap gap-4">
+        <Linkbox linkList={pageData} />
+      </div> -->
     </div>
   </div>
 {:else}
   <div
-    
     class="flex w-full h-full justify-center bg-[url('/background.webp')] bg-center bg-cover overflow-auto"
     class:background-img={user.backgroundVisibility}
     style="background-color: {user.backgroundColor}"
   >
-    <div class="flex flex-col container 2xl:self-center">
+    <div class="flex flex-col container xl:self-center">
       <div class="w-full flex self-start text-4xl text-gray-400">
         <Searchbar placeholder="Search..." />
       </div>
@@ -46,7 +62,7 @@
         {#if user.rssVisibility}
           <div class="flex flex-col max-w-3xl grow gap-4 overflow-hidden">
             {#if data.valid}
-              <Rss data={data.items} />
+              <Rss minimal={false} data={data.items} />
             {:else}
               <p>
                 Problem with rss config!
