b5b5493e35e8fadc9861bc5c83035aa1b8597c07
Walter Leeper <wleeper13@outlook.com>
2024-03-05 12:26:11 -0600

    removed function in run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env sh
 
-# apply migrations or build out db
-function migrate() {
-    mkdir -p prisma
-    mv schema.prisma prisma
-    mv migrations prisma
-    npx prisma migrate deploy
-    npx prisma db push
-    mv prisma/schema.prisma .
-    mv prisma/migrations .
-}
+# move files for prisma workflow
+mkdir -p prisma
+mv schema.prisma prisma
+mv migrations prisma
 
 if test -f "prisma/prisma.db"; then
     echo "Found prisma.db"
-    migration_needed="$(npx prisma migrate diff --from-url 'file:./prisma/prisma.db' --to-migrations migrations)"
+    migration_needed="$(npx prisma migrate diff \
+        --from-url 'file:./prisma/prisma.db' \
+        --to-migrations prisma/migrations)"
     if [ "$migration_needed" = "No difference detected." ]; then
         echo "No migration needed"
     else
@@ -21,12 +17,16 @@ if test -f "prisma/prisma.db"; then
         echo "Backing up prisma.db"
         cp "prisma/prisma.db" "prisma/prisma.$(date -u +%Y-%m-%dT%H-%M-%S%Z).db"
         echo "Beginning migration"
-        migrate
+        npx prisma migrate deploy
     fi
 else
     echo "Generating prisma.db"
-    migrate
 fi
+npx prisma db push
+
+# return files to container
+mv prisma/schema.prisma .
+mv prisma/migrations .
 
 # Start server
 node index.js
